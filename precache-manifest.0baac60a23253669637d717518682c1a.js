f8d1a240493e16eb048eaaed987f90a6d228cc7e
Jose Bedoya <jossephb@gmail.com>
2020-04-08 19:58:27 -0500

    Updates

---
+++ precache-manifest.0baac60a23253669637d717518682c1a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e1f51608b35e7398bebf5f9be7f3194",
+    "url": "/covid-19-react/index.html"
+  },
+  {
+    "revision": "2b1f982d7295d31bc3d3",
+    "url": "/covid-19-react/static/css/main.4f9d2623.chunk.css"
+  },
+  {
+    "revision": "5408a5dea54434d81da1",
+    "url": "/covid-19-react/static/js/2.ae93f6e1.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19-react/static/js/2.ae93f6e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b1f982d7295d31bc3d3",
+    "url": "/covid-19-react/static/js/main.1abf351e.chunk.js"
+  },
+  {
+    "revision": "35950ca88005d5f28445",
+    "url": "/covid-19-react/static/js/runtime-main.d0aca3e6.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19-react/static/media/image.d7265326.png"
+  }
+]);
